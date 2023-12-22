c764e0abb5d82cde6e7e39720d217e57b0a0a088
Gessé Silva <gesse.silva@gmail.com>
2023-12-22 17:07:18 -0300

    Adicionando página 'sobre'

---
+++ pages/sobre.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.title('Sobre')
+
+
+st.header('Trabalho final da disciplina Machine Learning Hands On', divider='grey')
+st.subheader('Especialização em Data Science & Analytics Senai/CIMATEC')
+
+st.write('\n\n\n')
+
+st.markdown(
+"""
+Equipe:
+- Gessé Pinto da Silva
+- Yuri Gonzalez Araújo
+"""
+)
